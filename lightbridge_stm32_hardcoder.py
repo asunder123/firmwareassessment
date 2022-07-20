6257b1f8f9fdef0b7477d97932dd4de3b22b8029
Anand Sunder <50335583+asunder123@users.noreply.github.com>
2022-07-20 15:37:42 +0530

    Update lightbridge_stm32_hardcoder.py
    removing hard coded changes

--- lightbridge_stm32_hardcoder.py
+++ lightbridge_stm32_hardcoder.py
@@ -135,6 +135,8 @@ import collections.abc
 import itertools
 import enum
 import json
+import configparser
+config = configparser.ConfigParser()
 
 
 sys.path.insert(0, './')
@@ -4970,7 +4972,7 @@ loc_F3B6:
   'lb_mcu_version_1':	{'type': VarType.RELATIVE_ADDR_TO_GLOBAL_DATA, 'baseaddr': "PC+", 'variety': DataVariety.INT32_T,
     'public': "og_hardcoded.lightbridge_stm32", 'depend': "mcu_firmware_version", 'getter': version_string_to_int_getter},
   'mcu_firmware_version':	{'type': VarType.DETACHED_DATA, 'variety': DataVariety.CHAR, 'array': 11,
-    'public': "og_hardcoded.lightbridge_stm32", 'minValue': "00.00.00.00", 'maxValue': "99.99.99.99",
+    'public': "og_hardcoded.lightbridge_stm32", 'minValue': "00.00.00.00", 'maxValue': config.get("99.99.99.99"),
     'depend': "lb_mcu_version_1", 'getter': version_int_to_string_getter, 'forceVisible': True,
     'description': "Firmware version number"},
 },
